999298873bea66077e7bd8a20ce8b127e2771389
Cristian Pineda Leon <c.d.pineda96@gmail.com>
2020-06-10 21:46:30 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@ index = 0
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-    if board[index] == " "
+    if board[index] == " " && ""
       false
     elsif board[index] == "X" && "O"
       true
