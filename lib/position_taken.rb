7035a22420b80f750fa92cf50069af8faa0f4f44
Cristian Pineda Leon <c.d.pineda96@gmail.com>
2020-06-10 21:47:09 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,6 +5,8 @@ index = 0
 def position_taken?(board, index)
     if board[index] == " " && ""
       false
+    elsif board[index] == nil
+      false
     elsif board[index] == "X" && "O"
       true
     end
