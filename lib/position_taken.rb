87de107cd310244b72d013a816eddfcef9162774
Cristian Pineda Leon <c.d.pineda96@gmail.com>
2020-06-10 22:02:09 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,18 +2,6 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 index = 0
 # code your #position_taken? method here!
 
-#def position_taken?(board, index)
-#    if board[index] == " "
-#      false
-#    elsif board[index] == ""
-#      false
-#    elsif board[index] == nil
-#      false
-#    elsif board[index] == "X" && "O"
-#      true
-#    end
-#end
-
 def position_taken?(board, index)
   if board[index] == " "
     false
