76f3cb792b9235656eb9628af24733e21a650c68
Cristian Pineda Leon <c.d.pineda96@gmail.com>
2020-06-10 22:00:11 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -15,13 +15,13 @@ index = 0
 #end
 
 def position_taken?(board, index)
-  if board[index] == "X" || "O"
-    true
-  elsif board[index] == " "
+  if board[index] == " "
     false
   elsif board[index] == ""
     false
   elsif board[index] == nil
     false
+  elsif board[index] == "X" || "O"
+    true
   end
 end
