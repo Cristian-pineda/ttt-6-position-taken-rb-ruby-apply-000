b5cf60d799c61db7f30aaa8ab3cd7fd330f2c7b9
Cristian Pineda Leon <c.d.pineda96@gmail.com>
2020-06-10 21:30:48 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,7 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-index = num
 
 # code your #position_taken? method here!
 
 def position_take?(board, index)
-  if board[index] == " "
-    true
+    board[index]
 end
